77a27b70b9e0e8cc31994a6e5f84d0e751194ac5
ras-indo <celengan1dana@gmail.com>
2026-02-01 03:31:06 +0700

    modification

--- b.sh
+++ b.sh
@@ -1,3 +1,6 @@
+sudo modprobe -r 8189fs   # unload modul dari kernel
+sudo rm /lib/modules/$(uname -r)/kernel/drivers/net/wireless/8189fs.ko
+sudo depmod -a            # rebuild database modul
 make -j4 ARCH=arm64 KSRC=/lib/modules/$(uname -r)/build
 sudo cp 8189fs.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless/
 depmod -a
